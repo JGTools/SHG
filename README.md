46a5f0ee7d6c8d4ba617c280539f894c6a2fa6e9
f <eribra@kth.se>
2022-12-21 11:29:00 +0100

    setup

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # SHG
+
+[![npm](https://img.shields.io/npm/v/@jgtools/shg)](https://www.npmjs.com/package/@jgtools/shg)
+[![npm](https://img.shields.io/npm/dm/@jgtools/shg)](https://www.npmjs.com/package/@jgtools/shg)
+[![GitHub](https://img.shields.io/github/license/jgtools/shg)](https://github.com/git/git-scm.com/blob/main/MIT-LICENSE.txt)
+
 2D Spatial Hash Grid
+
+## Features
+
+- :heavy_check_mark: Elements are stored on a 2D grid
+- :heavy_check_mark: Filter nearby elements
+- :blue_square: Written in TypeScript
+
+## Installation
+
+### Using npm
+
+```bash
+npm i @jgtools/shg
+```
+
+### Using cdn
+
+```html
+<script type="module">
+    import SHG from "https://cdn.jsdelivr.net/npm/@jgtools/shg@1.0.0/dist/index.min.js";
+    // ...
+</script>
+```
+
+## Usage
+
+```javascript
+```
+
+## License
+
+MIT
---
+++ src/index.ts
@@ -0,0 +1,69 @@
+/**
+ * SHG - Spatial Hash Grid
+ */
+export default class SHG<ID> {
+    #cells: Map<number, Set<ID>> = new Map<number, Set<ID>>();
+    #hashes: Map<ID, number> = new Map<ID, number>();
+    #data = { cs: 1, cx: 1, cy: 1 };
+    constructor(w: number, h: number, cs: number) {
+        this.#data.cs = cs;
+        this.#data.cx = Math.floor(w / cs) + 1;
+        this.#data.cy = Math.floor(h / cs) + 1;
+
+        for (let y = 0; y < this.#data.cy; y++) {
+            for (let x = 0; x < this.#data.cx; x++) {
+                this.#cells.set(this.#getCellHash(x, y), new Set<ID>());
+            }
+        }
+    }
+    set(id: ID, x: number, y: number) {
+        const [cx, cy] = this.#getCellPos(x, y);
+        const hash = this.#getCellHash(cx, cy);
+        this.#setInner(id, hash);
+    }
+    #setInner(id: ID, hash: number) {
+        this.#cells.get(hash)?.add(id);
+        this.#hashes.set(id, hash);
+    }
+    delete(id: ID) {
+        const hash = this.#hashes.get(id);
+        if (hash === undefined)
+            return;
+        this.#cells.get(hash)?.delete(id);
+        this.#hashes.delete(id);
+    }
+    update(id: ID, x: number, y: number) {
+        const ph = this.#hashes.get(id);
+        if (ph === undefined)
+            return;
+        const [nx, ny] = this.#getCellPos(x, y);
+        const nh = this.#getCellHash(nx, ny);
+        if (nh == ph)
+            return;
+        this.delete(id);
+        this.#setInner(id, nh);
+    }
+    query(tx: number, ty: number, r: number): ID[] {
+        const [sx, sy] = this.#getCellPos(tx - r, ty - r);
+        const [ex, ey] = this.#getCellPos(tx + r, ty + r);
+
+        const ids: ID[] = [];
+        for (let y = sy; y <= ey; y++) {
+            for (let x = sx; x <= ex; x++) {
+                const cell = this.#cells.get(this.#getCellHash(x, y));
+                if (cell)
+                    ids.push(...cell);
+            }
+        }
+        return ids;
+    }
+    #getCellHash(x: number, y: number) {
+        return y * this.#data.cx + x;
+    }
+    #getCellPos(x: number, y: number): [number, number] {
+        const cx = this.#clamp(Math.floor(x / this.#data.cs), this.#data.cx - 1);
+        const cy = this.#clamp(Math.floor(y / this.#data.cs), this.#data.cy - 1);
+        return [cx, cy];
+    }
+    #clamp(n: number, max: number) { return Math.max(0, Math.min(n, max)) }
+}
